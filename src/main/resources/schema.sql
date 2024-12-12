41a7c3998d42c2459e5b71b309002ea6360b5bfd
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-12-12 19:49:32 +0300

    fix schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,11 +10,12 @@ CREATE TABLE IF NOT EXISTS mpa (
 	id BIGINT PRIMARY KEY,
 	name VARCHAR NOT NULL UNIQUE CHECK (UPPER(name) IN ('G', 'PG', 'PG-13', 'R', 'NC-17'))
 );
-MERGE INTO mpa KEY(ID) VALUES(1, 'G');
-MERGE INTO mpa KEY(ID) VALUES(2, 'PG');
-MERGE INTO mpa KEY(ID) VALUES(3, 'PG-13');
-MERGE INTO mpa KEY(ID) VALUES(4, 'R');
-MERGE INTO mpa KEY(ID) VALUES(5, 'NC-17');
+MERGE INTO mpa KEY(ID) VALUES
+(1, 'G'),
+(2, 'PG'),
+(3, 'PG-13'),
+(4, 'R'),
+(5, 'NC-17');
 
 CREATE TABLE IF NOT EXISTS films (
 	id BIGSERIAL PRIMARY KEY,
@@ -29,12 +30,13 @@ CREATE TABLE IF NOT EXISTS genres (
 	id BIGINT PRIMARY KEY,
 	name VARCHAR NOT NULL UNIQUE CHECK (LOWER(name) IN ('комедия', 'драма', 'мультфильм', 'триллер', 'документальный', 'боевик'))
 );
-MERGE INTO genres KEY(ID) VALUES(1, 'Комедия');
-MERGE INTO genres KEY(ID) VALUES(2, 'Драма');
-MERGE INTO genres KEY(ID) VALUES(3, 'Мультфильм');
-MERGE INTO genres KEY(ID) VALUES(4, 'Триллер');
-MERGE INTO genres KEY(ID) VALUES(5, 'Документальный');
-MERGE INTO genres KEY(ID) VALUES(6, 'Боевик');
+MERGE INTO genres KEY(ID) VALUES
+(1, 'Комедия'),
+(2, 'Драма'),
+(3, 'Мультфильм'),
+(4, 'Триллер'),
+(5, 'Документальный'),
+(6, 'Боевик');
 
 CREATE TABLE IF NOT EXISTS films_genres (
 	id BIGSERIAL PRIMARY KEY,
