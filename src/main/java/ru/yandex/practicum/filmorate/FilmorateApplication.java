e68e926c583dc804bffacb87a83a484db15a756e
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-12-13 22:22:10 +0300

    fix FilmorateApplicationa

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,21 +2,10 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FilmorateApplication {
-    private static ConfigurableApplicationContext ctx;
-
     public static void main(String[] args) {
-        run(args);
-    }
-
-    public static void run(String[] args) {
-        ctx = SpringApplication.run(FilmorateApplication.class, args);
-    }
-
-    public static void close() {
-        ctx.close();
+        SpringApplication.run(FilmorateApplication.class, args);
     }
 }
