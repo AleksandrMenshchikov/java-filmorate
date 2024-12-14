5dc507adede4dace86f5269dd2599fbe17ca7f47
AleksandrMenshchikov <aleksandr.menshchikov1982@gmail.com>
2024-12-14 10:38:46 +0300

    fix CentralExceptionHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/CentralExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/CentralExceptionHandler.java
@@ -2,20 +2,24 @@ package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.Builder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.yandex.practicum.filmorate.exception.BaseException;
 
 import java.time.Instant;
 
 @RestControllerAdvice
 public class CentralExceptionHandler {
+
+    @Builder
+    public record ErrorResponse(
+            Instant timestamp,
+            Integer status,
+            String error,
+            String message,
+            String path) {
+    }
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleCustomException(BaseException e, HttpServletRequest request) {
@@ -26,38 +30,5 @@ public class CentralExceptionHandler {
                 .message(e.getMessage())
                 .path(request.getRequestURI())
                 .build());
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleInternalException(Exception e, HttpServletRequest request) {
-        e.printStackTrace();
-        HttpStatus httpStatus;
-
-        if (e instanceof MethodArgumentNotValidException || e instanceof MethodArgumentTypeMismatchException) {
-            httpStatus = HttpStatus.BAD_REQUEST;
-        } else if (e instanceof NoResourceFoundException) {
-            httpStatus = HttpStatus.NOT_FOUND;
-        } else if (e instanceof HttpRequestMethodNotSupportedException) {
-            httpStatus = HttpStatus.METHOD_NOT_ALLOWED;
-        } else {
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        }
-
-        return ResponseEntity.status(httpStatus.value()).body(ErrorResponse.builder()
-                .timestamp(Instant.now())
-                .status(httpStatus.value())
-                .error(httpStatus.getReasonPhrase())
-                .message(e.getMessage())
-                .path(request.getRequestURI())
-                .build());
-    }
-
-    @Builder
-    public record ErrorResponse(
-            Instant timestamp,
-            Integer status,
-            String error,
-            String message,
-            String path) {
     }
 }
